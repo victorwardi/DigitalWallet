6f9fddeb84884ea1d803d0de37df6e4b637f375e
Victor Wardi <victorcaetano@live.com>
2019-02-19 16:13:21 -0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,164 @@
+/**
+ * Created by Victor Wardi - @vwardi on 19/02/2019
+ **/
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+/*
+ * Create TransactionException, DigitalWallet, and DigitalWalletTransaction classes here.
+ */
+
+
+class TransactionException extends Exception{
+
+    private String errorMessage;
+    private String errorCode;
+
+    public TransactionException(String errorMessage, String errorCode) {
+        this.errorMessage = errorMessage;
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+}
+
+class DigitalWallet{
+
+    private String walletId;
+    private String userName;
+    private String userAccessCode;
+    private int walletBalance;
+
+    public DigitalWallet(String walletId, String userName) {
+        this.walletId = walletId;
+        this.userName = userName;
+    }
+
+    public DigitalWallet(String walletId, String userName, String userAccessCode) {
+        this.walletId = walletId;
+        this.userName = userName;
+        this.userAccessCode = userAccessCode;
+    }
+
+    public String getWalletId() {
+        return walletId;
+    }
+
+    public void setWalletId(String walletId) {
+        this.walletId = walletId;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAccessCode() {
+        return userAccessCode;
+    }
+
+    public void setUserAccessCode(String userAccessCode) {
+        this.userAccessCode = userAccessCode;
+    }
+
+    public String getUserAccessToken() {
+        return userAccessCode;
+    }
+
+    public int getWalletBalance() {
+        return walletBalance;
+    }
+
+    public void setWalletBalance(int walletBalance) {
+        this.walletBalance = walletBalance;
+    }
+}
+
+class DigitalWalletTransaction{
+
+
+    public void addMoney(DigitalWallet digitalWallet, int amount) throws TransactionException{
+
+    }
+
+    public void payMoney(DigitalWallet digitalWallet, int amount) throws TransactionException{
+
+    }
+
+}
+
+public class Solution {
+    private static final Scanner INPUT_READER = new Scanner(System.in);
+    private static final DigitalWalletTransaction DIGITAL_WALLET_TRANSACTION = new DigitalWalletTransaction();
+
+    private static final Map<String, DigitalWallet> DIGITAL_WALLETS = new HashMap<>();
+
+    public static void main(String[] args) {
+        int numberOfWallets = Integer.parseInt(INPUT_READER.nextLine());
+        while (numberOfWallets-- > 0) {
+            String[] wallet = INPUT_READER.nextLine().split(" ");
+            DigitalWallet digitalWallet;
+
+            if (wallet.length == 2) {
+                digitalWallet = new DigitalWallet(wallet[0], wallet[1]);
+            } else {
+                digitalWallet = new DigitalWallet(wallet[0], wallet[1], wallet[2]);
+            }
+
+            DIGITAL_WALLETS.put(wallet[0], digitalWallet);
+        }
+
+        int numberOfTransactions = Integer.parseInt(INPUT_READER.nextLine());
+        while (numberOfTransactions-- > 0) {
+            String[] transaction = INPUT_READER.nextLine().split(" ");
+            DigitalWallet digitalWallet = DIGITAL_WALLETS.get(transaction[0]);
+
+            if (transaction[1].equals("add")) {
+                try {
+                    DIGITAL_WALLET_TRANSACTION.addMoney(digitalWallet, Integer.parseInt(transaction[2]));
+                    System.out.println("Wallet successfully credited.");
+                } catch (TransactionException ex) {
+                    System.out.println(ex.getErrorCode() + ": " + ex.getMessage() + ".");
+                }
+            } else {
+                try {
+                    DIGITAL_WALLET_TRANSACTION.payMoney(digitalWallet, Integer.parseInt(transaction[2]));
+                    System.out.println("Wallet successfully debited.");
+                } catch (TransactionException ex) {
+                    System.out.println(ex.getErrorCode() + ": " + ex.getMessage() + ".");
+                }
+            }
+        }
+
+        System.out.println();
+
+        DIGITAL_WALLETS.keySet()
+                .stream()
+                .sorted()
+                .map((digitalWalletId) -> DIGITAL_WALLETS.get(digitalWalletId))
+                .forEachOrdered((digitalWallet) -> {
+                    System.out.println(digitalWallet.getWalletId()
+                            + " " + digitalWallet.getUsername()
+                            + " " + digitalWallet.getWalletBalance());
+                });
+    }
+}
